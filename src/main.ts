647aed4e68d6bf6eff5437f883aa8bc22f8bc95b
ZaneShan <dchen113@ucsc.edu>
2024-11-03 18:02:59 -0800

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 // todo
+
+import "./style.css";
+
+const title = document.createElement("h1");
+title.textContent = "Geocoin Carrier";
+document.body.append(title);
+
+// button
+const button = document.createElement("button");
+button.textContent = "button";
+document.body.append(button);
+// button functionality
+button.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
